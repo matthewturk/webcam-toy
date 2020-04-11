d440fc5fa9b998085b7a51a298a87eda261fd2bd
Matthew Turk <matthewturk@gmail.com>
2020-04-11 14:24:30 -0500

    add a color-offset toy

--- webcam-toy.py
+++ webcam-toy.py
@@ -18,18 +18,31 @@ def pixelize_frame(input_arr, output_arr):
     for i in range(input_arr.shape[0]):
         for j in range(input_arr.shape[1]):
             for k in range(input_arr.shape[2]):
-                output_arr[i, j, k] = input_arr[(i // pixel_size) * pixel_size,
-                                                (j // pixel_size) * pixel_size, k]
+                output_arr[i, j, 2 - k] = input_arr[(i // pixel_size) * pixel_size,
+                                                    (j // pixel_size) * pixel_size,
+                                                    k]
+
+@numba.jit(nopython=True)
+def color_offset_frame(input_arr, output_arr):
+    for i in range(input_arr.shape[0]):
+        for j in range(input_arr.shape[1]):
+            for k in range(input_arr.shape[2]):
+                i1 = (i + pixel_size*k) % input_arr.shape[0]
+                output_arr[i, j, 2 - k] = input_arr[i1, j, k]
 
 output_arr = np.zeros((480, 640, 3), dtype="u1")
 
+N_frames = 300
 frame_i = 0
+t0 = time.time()
 while True:
-    if frame_i % 300 == 0:
-        print(f"Rendering frame {frame_i}")
+    if frame_i % N_frames == 0:
+        fps = N_frames / (time.time() - t0)
+        t0 = time.time()
+        print(f"Rendering frame {frame_i} with FPS of {fps}")
     frame_i += 1
     _, frame = in_cam.read()
-    pixelize_frame(frame, output_arr)
-    out_frame = cv2.cvtColor(output_arr, cv2.COLOR_BGR2RGB)
-    out_cam.schedule_frame(out_frame)
-    time.sleep(1/30.0)
+    #pixelize_frame(frame, output_arr)
+    color_offset_frame(frame, output_arr)
+    out_cam.schedule_frame(output_arr)
+    #time.sleep(1/30.0)
